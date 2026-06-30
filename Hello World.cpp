09846e6410383760fe8cb52e95ec7319a04c580f
jmharris-source <jasmineharris2011@gmail.com>
2026-06-30 13:37:15 +0000

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,18 @@
+/*
+Developer: Jasmine Harris
+Date: May 7, 2026
+Course: CS 210
+Purpose: This program displays "Hello World!" to the user
+*/
+
+#include <iostream> // Allows the program to use input and outpu commands
+
+using namespace std;
+
+int main() {
+	//Display the message to the screen
+	cout << "Hello World!" << endl;
+
+	// End the program successfully
+	return 0;
+}
---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Developer: Jasmine Harris
+Date: May 7, 2026
+Course: CS 210
+Purpose: This program displays the message "Hello, World!" to the user
+"""
+
+# Display the message to the screen
+print("Hello World!")
